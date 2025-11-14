0fd4f9d980e9dbbe5eb7c2b7eab2eaa37c9d4f6e
Baptiste Candela <baptiste.candela@gmail.com>
2025-11-14 21:42:34 +0100

    init

---
+++ src/db/execDB.js
@@ -0,0 +1,31 @@
+import sqlite3 from 'sqlite3';
+import fs from 'fs';
+
+const db = new sqlite3.Database("../../dist/db/database.db", (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connexion à la base de données SQLite réussie.');
+});
+
+const request = "./Request.sql"
+
+fs.readFile(request, 'utf8', (err, data) => {
+    if (err) {
+        throw err;
+    }
+    const requetes = data.toString().split(';');
+    if(requetes[requetes.length-1] == "") requetes.pop();
+    db.serialize(() => {
+        requetes.forEach(requete => {
+            db.all(requete, [], (err, rows) => {
+                if (err) {
+                    throw err
+                }
+                console.log(rows);
+            }); 
+        })
+    });
+    db.close();
+});
+
